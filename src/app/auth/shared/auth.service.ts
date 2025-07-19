00bc167c987751010c750de6cf39ba8747fa72e8
Mwakazi_eric <mwakazieric@gmail.com>
2025-07-19 15:23:46 +0300

    added api endpoints and env setup

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -6,7 +6,7 @@ import { LocalStorageService } from 'ngx-webstorage';
 import { LoginRequestPayload } from '../login/login-request.payload';
 import { LoginResponse } from '../login/login-response.payload';
 import { map, tap } from 'rxjs/operators';
-import { environment } from '../../../environments/environment';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
--- src/app/shared/vote.service.ts
+++ src/app/shared/vote.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { VotePayload } from './vote-button/vote-payload';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +11,8 @@ import { Observable } from 'rxjs';
 export class VoteService {
 
   constructor(private http: HttpClient) { }
-
+  private apiUrl = environment.BASE_API_URI;
   vote(votePayload: VotePayload): Observable<any> {
-    return this.http.post('http://localhost:8080/api/votes/', votePayload);
+    return this.http.post(`${this.apiUrl}/votes/`, votePayload);
   }
 }
